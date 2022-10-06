32c2d39d183ff36660c501cf2240fe1a5759589a
Tushar <0tushar0@gmail.com>
2022-10-06 17:56:36 -0500

    Iterating struct and class using char ptr

---
+++ struct-class.cpp
@@ -0,0 +1,54 @@
+#include <cstdio>
+#include <iostream>
+
+struct Foo {
+	double a = 3.14;
+	int b = 42;
+	std::string c = "Hello world";
+
+	Foo() = default;
+
+	void print() {
+		std::cout << b << ' ' << c << ' ' << a << '\n';
+	}
+};
+
+class Bar {
+private:
+	double a = 3.14;
+	int b = 42;
+	std::string c = "Hello world";
+
+public:
+	Bar() = default;
+
+	void print() {
+		std::cout << b << ' ' << c << ' ' << a << '\n';
+	}
+};
+
+
+int main() {
+	Foo f;
+	Bar b;
+	char *ptr = (char*)(&f);
+	int i = 0;
+
+	f.print();
+	std::cout << "Iterating Foo\n";
+	for(; i < sizeof(Foo); i++) {
+		printf("%x  ", *(ptr + i));
+	}
+
+	std::cout << "\n\n";
+
+	b.print();
+	std::cout << "Iterating Bar\n";
+	ptr = (char*)(&b);
+	for(i = 0; i < sizeof(Bar); i++) {
+		printf("%x  ", *(ptr + i));
+	}
+
+	std::cout << '\n';
+	return 0;
+}
